e00477e8892703b2316abb416c41cd88fa9183d6
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2024-03-28 21:01:14 +0300

    new changes

---
+++ hello/target/debug/deps/hello-4521f77a1a9b58cf.d
@@ -0,0 +1,5 @@
+/home/cecilia/rust_learning/hello/target/debug/deps/libhello-4521f77a1a9b58cf.rmeta: src/main.rs
+
+/home/cecilia/rust_learning/hello/target/debug/deps/hello-4521f77a1a9b58cf.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/deps/hello-e662b455ebbd16c7.d
@@ -0,0 +1,5 @@
+/home/cecilia/rust_learning/hello/target/debug/deps/libhello-e662b455ebbd16c7.rmeta: src/main.rs
+
+/home/cecilia/rust_learning/hello/target/debug/deps/hello-e662b455ebbd16c7.d: src/main.rs
+
+src/main.rs:
