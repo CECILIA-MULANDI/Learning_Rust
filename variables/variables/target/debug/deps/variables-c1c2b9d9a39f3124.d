f1f32bc74ab66c00c39e8530be43890acc71b89d
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2024-03-29 11:26:24 +0300

    variables

---
+++ variables/variables/target/debug/deps/variables-c1c2b9d9a39f3124.d
@@ -0,0 +1,5 @@
+/home/cecilia/rust_learning/variables/variables/target/debug/deps/variables-c1c2b9d9a39f3124: src/main.rs
+
+/home/cecilia/rust_learning/variables/variables/target/debug/deps/variables-c1c2b9d9a39f3124.d: src/main.rs
+
+src/main.rs:
---
+++ variables/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/cecilia/rust_learning/variables/variables/target/debug/variables: /home/cecilia/rust_learning/variables/variables/src/main.rs
